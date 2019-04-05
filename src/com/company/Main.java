39e2e71e43c364c74b6a576f490c2deccd4fba18
KBarons <KBarons@gmail.com>
2019-04-05 20:31:22 +0300

    Task3-finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello user! Can you write your name, please?");
+        int i = 1, n = 40, t1 = 0, t2 = 1;
+        System.out.print("First " + n + " terms: ");
 
-        Scanner sc = new Scanner(System.in);
-        String name = sc.next();
-        System.out.println(name + ", we kindly invite you to the grand opening ceremony!");
+        while (i <= n)
+        {
+            System.out.print(t1 + " + ");
+
+            int sum = t1 + t2;
+            t1 = t2;
+            t2 = sum;
+
+            i++;
+        }
     }
 }
