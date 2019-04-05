8971a108a64b650a610a9270ceb2be3ab4eae1bb
KBarons <KBarons@gmail.com>
2019-04-05 20:23:17 +0300

    Task1-finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello user! Can you write your name, please?");
+
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println(name + ", we kindly invite you to the grand opening ceremony!");
     }
 }
